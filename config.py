7e997322b18c4e6ee925c47ff3ad2e99aaf4cf8d
Nelson Dufitimana <nelsondft10@gmail.com>
2022-02-06 15:45:31 -0500

    heroku deployment

--- config.py
+++ config.py
@@ -3,9 +3,12 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__)) #stores the main directory name
 class Config(object):
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', '').replace(
+        'postgres://', 'postgresql://') or \
         'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
+    LOG_TO_STDOUT = os.environ.get('LOG_TO_STDOUT')
+
     """ 
     The following section allows receiving 
     notifications when the app encouters an error while
